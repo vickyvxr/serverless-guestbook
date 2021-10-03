9756e7e7a7c2d010e4ef4dcbc10dc0d20716d14a
vickymr <33089634+vickyvxr@users.noreply.github.com>
2021-10-03 07:51:06 +0200

    URI

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f39a9d48.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
